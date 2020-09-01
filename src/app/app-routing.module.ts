980784a292e34a3bdb89797290934b5b31d395b9
Alejandro Gallo <alegallo@gmail.com>
2020-08-31 22:01:27 -0300

    Agregado de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { BienvenidoComponent } from './components/bienvenido/bienvenido.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'bienvenido', component: BienvenidoComponent },
+  { path: 'error', component: ErrorComponent },
+  { path: '', redirectTo: '/error', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
